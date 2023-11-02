1016c34e3348902298440d0101cec5b4a84739a4
ferid <afe681a2@gamil.com>
2023-11-03 02:45:55 +0400

    s

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 // Belirli alan adlarından gelen isteklere izin ver
-const allowedOrigins = ['https://example.com', 'https://another-example.com'];
+const allowedOrigins = ['https://oooobombakimi.netlify.app/', 'https://another-example.com'];
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -21,3 +21,9 @@ app.use(cors({
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+
+app.get('/api/data', (req, res) => {
+    res.header('Access-Control-Allow-Origin', 'https://oooobombakimi.netlify.app/');
+    res.json({ your: 'data' });
+  });
